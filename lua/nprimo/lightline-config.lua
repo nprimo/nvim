4ed953a40c6bfac07bfe3891df3f45bd11ef9f61
nprimo <primo.niccolo@gmail.com>
2022-11-15 13:15:32 +0000

    update lightline-config: show relative path

--- lua/nprimo/lightline-config.lua
+++ lua/nprimo/lightline-config.lua
@@ -2,7 +2,7 @@ vim.cmd([[
 let g:lightline = {
 \ 'active': {
 \   'left': [ [ 'mode', 'paste' ],
-\             [ 'gitbranch', 'readonly', 'filename', 'modified' ] ]
+\             [ 'gitbranch', 'readonly', 'relativepath', 'modified' ] ]
 \ },
 \ 'component_function': {
 \   'gitbranch': 'gitbranch#name'
